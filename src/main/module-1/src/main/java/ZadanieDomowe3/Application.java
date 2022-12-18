b857c2305ea2be5a93ae15e760ba717f5754c691
makos81 <maciejrajcic@gmail.com>
2022-12-18 20:01:17 +0100

    Zadanie 3

---
+++ src/main/module-1/src/main/java/ZadanieDomowe3/Application.java
@@ -0,0 +1,14 @@
+package ZadanieDomowe3;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Application {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newCachedThreadPool();
+        for (int n = 0; n < 400; n++) {
+            executor.submit(new FileManager());
+        }
+        executor.shutdown();
+    }
+}
---
+++ src/main/module-1/src/main/java/ZadanieDomowe3/FileManager.java
@@ -0,0 +1,30 @@
+package ZadanieDomowe3;
+
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+
+public class FileManager implements Runnable {
+    private static Semaphore semaphore = new Semaphore(10);
+
+    @Override
+    public void run() {
+        try {
+            semaphore.acquire();
+            readFile();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            semaphore.release();
+        }
+    }
+
+    private void readFile() throws InterruptedException {
+        Random random = new Random();
+        synchronized (FileManager.class) {
+            System.out.println("Reading file ");
+        }
+        Thread.sleep(random.nextInt(100));
+        synchronized (FileManager.class) {
+        }
+    }
+}
